1edbaf0f64f33c3a987a218b5390e73c1e3113de
Minirice <minirice2017@gmail.com>
2026-01-28 17:51:15 +0800

    Revert "修复bug"
    This reverts commit bf812ac7026e9dc1242ab463cd79bcf4b22184a5.

--- pages/chat/chat.js
+++ pages/chat/chat.js
@@ -531,6 +531,20 @@ Page({
     });
     
     this.startRecordingTimer();
+      },
+      fail: () => {
+        wx.showModal({
+          title: '提示',
+          content: '需要录音权限才能使用语音功能',
+          confirmText: '去设置',
+          success: (res) => {
+            if (res.confirm) {
+              wx.openSetting();
+            }
+          }
+        });
+      }
+    });
   },
 
   // 停止录音
